c470b73c3090902c92a21325f7424081cd67caf4
nimaiadepu <144487954+nimaiadepu@users.noreply.github.com>
2023-09-24 22:20:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as pltimport requests
+import requests 
+# Define your API key
+api_key = "YOUR_API_KEY"
+
+# Define the base URL for the UN Data API
+base_url = "http://data.un.org/Handlers/"
+
+# Define a function to fetch data from the API
+def fetch_un_data(indicator, country="all", year="2020"):
+    url = (
+        f"{base_url}{indicator}.ashx?"
+        f"DataSet=POP&crID={country}&sDM=YE&sp=0&" 
+        f"tid={indicator}TH&thid=1&ts=5&" 
+        f"rcID=1&occID=5&returnhtml=false&" 
+        f"token={api_key}"
+    )
+    response = requests.get(url)
+    data = response.json()
+    return data
+
 # Fetch population data
+
 population_data = fetch_un_data("P")
 df_population = pd.DataFrame(population_data["Data"])
 df_population.columns = ["Country", "Year", "Population"]
